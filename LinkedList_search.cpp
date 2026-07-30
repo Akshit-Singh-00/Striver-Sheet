4e9fe3c95e54ef21d66535231e6c0dbd7d5bb0f7
Akshit-Singh-00 <khush227799@gmail.com>
2026-07-30 18:28:31 +0530

    Day 13

---
+++ LinkedList_search.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int val){
+        data=val;
+        next=nullptr;
+    }
+};
+class solution{
+    public:
+    bool LinkedList_search(Node* head,int key){
+    Node* temp=head;
+    while(temp){
+        if(temp->data==key){
+            return true;
+        }
+        temp=temp->next;
+    }
+    return false;
+
+    }
+};
+
+int main(){
+    Node* first=new Node(10);
+        first->next=new Node(20);
+        first->next->next=new Node(30);
+        int k;
+        cin>>k;
+        solution obj;
+if (obj.LinkedList_search(first, k))
+    cout << "Element Found";
+else
+    cout << "Element Not Found";
+    return 0 ;
+}
