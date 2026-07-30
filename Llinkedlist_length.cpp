64a84fc69dbdb5c20f682f6453a0d10429045a43
Akshit-Singh-00 <khush227799@gmail.com>
2026-07-30 17:30:32 +0530

    Day 13

---
+++ Llinkedlist_length.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+public:
+    int data;
+    Node* next;
+    Node(int val){
+        data=val;
+        next=nullptr;
+    }
+};
+class solution{
+public:
+    int length_linkedist(Node* head){
+int count=0;
+Node* temp=head;
+while(temp){
+    count++;
+    temp=temp->next;
+}
+return count;
+    }
+};
+
+int main(){
+    Node* head=new Node(10);
+    head->next=new Node(20);
+    head->next->next=new Node(30);
+    solution obj;
+    cout<<"The length of linkedlist is - "<<obj.length_linkedist(head);
+    return 0 ;
+}
