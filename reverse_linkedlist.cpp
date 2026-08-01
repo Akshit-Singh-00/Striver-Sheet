3671def77092a0e0444512099408f12fb6bf161a
Akshit-Singh-00 <khush227799@gmail.com>
2026-08-01 19:38:10 +0530

    Day 14

--- reverse_linkedlist.cpp
+++ reverse_linkedlist.cpp
@@ -57,10 +57,73 @@ return head;
 
 }//t.c->O(2n) ; s.c->O(n)
 
+class Node2{
+public:
+    int data;
+    Node2* next;
+    Node2* prev;
+
+    Node2(int val){
+        data = val;
+        next = nullptr;
+        prev = nullptr;
+    }
+};
+
+Node2* convertDLL(vector<int> arr){
+
+    if(arr.empty()) return nullptr;
+
+    Node2* head = new Node2(arr[0]);
+    Node2* prev = head;
+
+    for(int i = 1; i < arr.size(); i++){
+
+        Node2* temp = new Node2(arr[i]);
+
+        prev->next = temp;
+        temp->prev = prev;
+
+        prev = temp;
+    }
+
+    return head;
+}
+
+Node2* ReverseDLL(Node2* head){
+
+    if(head == nullptr || head->next == nullptr)
+        return head;
+
+    Node2* curr = head;
+
+    while(curr != nullptr){
+
+        Node2* temp = curr->next;
+
+        curr->next = curr->prev;
+        curr->prev = temp;
+
+        head = curr;
+        curr = temp;
+    }
+
+    return head;
+}
+
+void print(Node2* head){
+
+    while(head){
+        cout << head->data << " ";
+        head = head->next;
+    }
+} // t.c->O(n);s.c->O(1)
+
+
 int main(){
       vector<int> arr = {12, 5, 8, 7, 4};
 
-    Node* head = ConvertArr2DLL(arr);
+    Node* head = ConvertArr2DLL(arr);//brute force approach
 
     cout << "Original DLL: ";
     print(head);
@@ -69,6 +132,18 @@ int main(){
 
     cout << "Reversed DLL: ";
     print(head);
+cout<<endl;
 
+
+Node2* head2=convertDLL(arr);//Optimal approcah;
+head2=ReverseDLL(head2);
+cout << "Original DLL: ";
+Node2* original = convertDLL(arr);
+print(original);
+
+head2 = ReverseDLL(original);
+cout<<endl;
+cout << "Reversed DLL: ";
+print(head2);
     return 0 ;
 }
