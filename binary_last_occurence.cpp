bed2d6af9f0b52cc4d67542c591dcafca8872181
Akshit-Singh-00 <khush227799@gmail.com>
2026-08-06 19:50:57 +0530

    Day 16

--- binary_last_occurence.cpp
+++ binary_last_occurence.cpp
@@ -12,10 +12,37 @@ int  brute_binary_last_occurence(vector<int> arr,int x){
     return n;
 }//t.c-O(n);s.c->O(1)
 
+
+int optimal_binary_last_occurence(vector<int> arr,int x){
+    int n=arr.size();
+    int left=0;
+    int right=n-1;
+    int ans=-1;
+    while (left<=right)
+    {
+        int mid=left+(right-left)/2;
+        if(arr[mid]==x){
+            ans=mid;
+            left=mid+1;
+        }
+        else if(x<arr[mid]){
+            right--;
+        }
+        else{
+            left++;
+        }
+    }
+    return ans;
+    
+}//t.c->O(logn) ;s.c->O(1)
+
 int main(){
     vector<int> arr={3, 4, 13, 13, 13, 20, 40} ;
     int x=13;
     int result1=brute_binary_last_occurence(arr,x);
     cout<<result1<<endl;
+
+    int result12=optimal_binary_last_occurence(arr,x);
+    cout<<result12;
     return 0 ;
 }
