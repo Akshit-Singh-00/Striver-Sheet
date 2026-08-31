afc81bdd897b6f3ae7f9a5a4bb685d6585257506
Akshit-Singh-00 <khush227799@gmail.com>
2026-08-31 19:05:53 +0530

    Day 23

--- Binary_search_Single_Element.cpp
+++ Binary_search_Single_Element.cpp
@@ -10,11 +10,31 @@ int brute_BS_Single_Element(vector<int> arr){
     }
     return ans;
     
-}
+}//t.c->O(n);s.c->O(1)
+
+int optimal__BS_Single_Element(vector<int> arr){
+     int low = 0, high = arr.size() - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            // Make mid even
+            if (mid % 2 == 1)
+                mid--;
+
+            if (arr[mid] == arr[mid + 1])
+                low = mid + 2;
+            else
+                high = mid;
+        }
+
+        return arr[low];
+}//t.c->O(logn);s.c->O(1)
 
 int main(){
-    vector<int> a={1,2,3,4,1,2,3,5,4};
+    vector<int> a={1,1,2,2,3,3,4,5,5,6,6};
     cout<<"element is - "<<brute_BS_Single_Element(a)<<endl;
+    cout<<"Element is  - "<<optimal__BS_Single_Element(a);
 
     return 0 ;
 }
