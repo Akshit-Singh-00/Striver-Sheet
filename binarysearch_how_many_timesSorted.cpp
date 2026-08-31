d3d0097066533090caa09bb7c8e0e9df0c09ea93
Akshit-Singh-00 <khush227799@gmail.com>
2026-08-31 18:44:58 +0530

    Day 23

--- binarysearch_how_many_timesSorted.cpp
+++ binarysearch_how_many_timesSorted.cpp
@@ -14,10 +14,24 @@ int brute_BS_how_manyTimes_sorted(vector<int> arr){
         }
     }
     return minIndex;
+}//t.c->O(n);s.c->O(1)
+
+int better_BS_how_manyTimes_sorted(vector<int> arr){
+    int n=arr.size();
+    for (int i = 0; i < n-1; i++)
+    {
+        if(arr[i]>arr[i+1]){
+            return i+1;
+        }
+    }
+    return -1;
+    
 }
 
 int main(){
     vector<int> arr = {4,5,6,7,0,1,2,3};
-    cout<<" result-> "<<brute_BS_how_manyTimes_sorted(arr);
+    cout<<" result-> "<<brute_BS_how_manyTimes_sorted(arr)<<endl;
+    cout<<" result-> "<<better_BS_how_manyTimes_sorted(arr)<<endl;
+
     return 0 ;
 }
