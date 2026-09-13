95e8706692aa5ad3df6b390939bb26fcc83b2b9d
Akshit-Singh-00 <khush227799@gmail.com>
2026-09-13 18:44:37 +0530

    Day 25

---
+++ optimal_LL_loop.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int val;
+    Node* next;
+    Node(int data){
+        val=data;
+        next=nullptr;
+    }
+};
+
+class sol{
+public:
+    bool findloop(Node* head){
+        Node* temp=head;
+        Node* slow=head;
+        Node* fast=head;
+       while (fast!=NULL || fast->next!=NULL)
+       {
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast){
+            return true;
+        }
+       }
+       return false;
+    }
+
+};//t.c->O(n);s.c->O(1)
+
+int main(){
+    Node *head = new  Node(1);
+    Node *second = new  Node(2);
+    Node *third = new  Node(3);
+    Node *fourth = new Node(4);
+    Node *fifth = new  Node(5);
+
+    head->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = fifth;
+  
+    fifth->next = third;
+
+    sol soll;
+
+   
+    if (soll.findloop(head)) {
+        std::cout << "Loop detected in the linked list." << std::endl;
+    } else {
+        std::cout << "No loop detected in the linked list." << std::endl;
+    }
+
+   
+    delete head;
+    delete second;
+    delete third;
+    delete fourth;
+    delete fifth;
+
+    return 0 ;
+}
